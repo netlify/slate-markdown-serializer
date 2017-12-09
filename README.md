6e6bbd5651c5b9da7a334604b45895024ac7cdd6
Tom Moor <tom.moor@gmail.com>
2017-12-08 19:19:07 -0800

    Update README Add additional tests for empty strings

--- README.md
+++ README.md
@@ -1,44 +1,82 @@
-[![CircleCI](https://circleci.com/gh/tommoor/slate-markdown-serializer.svg?style=svg)](https://circleci.com/gh/tommoor/slate-markdown-serializer)
+[![CircleCI](https://circleci.com/gh/tommoor/slate-md-serializer.svg?style=svg)](https://circleci.com/gh/tommoor/slate-md-serializer)
 
 # Slate Markdown Serializer
 
-A Markdown serializer for the [Slate Editor](http://slatejs.org).
+A Markdown serializer for the [Slate Editor](http://slatejs.org). Requires Slate 0.29+.
 
 
 ## Schema
 
-This module expects your Slate schema to have the following keys (example implementations included):
+This serializer expects your Slate nodes and marks to have the following keys (example implementations included):
 
 ```javascript
-{
-  marks: {
-    bold: props => <strong>{props.children}</strong>,
-    code: props => <code>{props.children}</code>,
-    italic: props => <em>{props.children}</em>,
-    underlined: props => <u>{props.children}</u>,
-    deleted: props => <del>{props.children}</del>,
-    added: props => <span>{props.children}</span>,
-  },
-  nodes: {
-    paragraph: props => <p>{props.children}</p>,
-    'block-quote': props => <blockquote>{props.children}</blockquote>,
-    'horizontal-rule': props => <hr />,
-    'bulleted-list': props => <ul>{props.children}</ul>,
-    'ordered-list': props => <ol>{props.children}</ol>,
-    'todo-list': props => <ul>{props.children}</ul>,
-    'list-item': props => <li>{props.children}</li>,
-    image: props => <img src={props.src} title={props.title} />,
-    link: props => <a href={props.href}>{props.children}</a>,
-    table: props => <table>{props.children}</table>,
-    'table-row': props => <tr>{props.children}</tr>,
-    'table-head': props => <th>{props.children}</th>,
-    'table-cell': props => <td>{props.children}</td>,
-    heading1: props => <h1>{props.children}</h1>,
-    heading2: props => <h2>{props.children}</h2>,
-    heading3: props => <h3>{props.children}</h3>,
-    heading4: props => <h4>{props.children}</h4>,
-    heading5: props => <h5>{props.children}</h5>,
-    heading6: props => <h6>{props.children}</h6>,
+function renderMark(props: Props) {
+  switch (props.mark.type) {
+    case 'bold':
+      return <strong>{props.children}</strong>;
+    case 'code':
+      return <Code>{props.children}</Code>;
+    case 'italic':
+      return <em>{props.children}</em>;
+    case 'underlined':
+      return <u>{props.children}</u>;
+    case 'deleted':
+      return <del>{props.children}</del>;
+    case 'added':
+      return <mark>{props.children}</mark>;
+    default:
   }
+}
+```
+
+```javascript
+export default function createRenderNode({ onInsertImage }: Options) {
+  return function renderNode(props: SlateNodeProps) {
+    const { attributes } = props;
+
+    switch (props.node.type) {
+      case 'paragraph':
+        return <Paragraph {...props} />;
+      case 'block-quote':
+        return <blockquote {...attributes}>{props.children}</blockquote>;
+      case 'bulleted-list':
+        return <ul {...attributes}>{props.children}</ul>;
+      case 'ordered-list':
+        return <ol {...attributes}>{props.children}</ol>;
+      case 'todo-list':
+        return <ul {...attributes}>{props.children}</ul>;
+      case 'table':
+        return <table {...attributes}>{props.children}</table>;
+      case 'table-row':
+        return <tr {...attributes}>{props.children}</tr>;
+      case 'table-head':
+        return <th {...attributes}>{props.children}</th>;
+      case 'table-cell':
+        return <td {...attributes}>{props.children}</td>;
+      case 'list-item':
+        return <li {...attributes}>{props.children}</li>;
+      case 'horizontal-rule':
+        return <hr />;
+      case 'code':
+        return <code {...attributes}>{props.children}</code>;
+      case 'image':
+        return <img src={props.src} title={props.title} />;
+      case 'link':
+        return <a href={props.href}>{props.children}</a>;
+      case 'heading1':
+        return <h1 {...attributes}>{props.children}</h1>;
+      case 'heading2':
+        return <h2 {...attributes}>{props.children}</h2>;
+      case 'heading3':
+        return <h3 {...attributes}>{props.children}</h3>;
+      case 'heading4':
+        return <h4 {...attributes}>{props.children}</h4>;
+      case 'heading5':
+        return <h5 {...attributes}>{props.children}</h5>;
+      case 'heading6':
+        return <h6 {...attributes}>{props.children}</h6>;
+      default:
+    }
+  };
 }
 ```
--- src/__tests__/renderer-test.js
+++ src/__tests__/renderer-test.js
@@ -2,39 +2,37 @@ import MarkdownRenderer from "../renderer";
 const Markdown = new MarkdownRenderer();
 
 test("parses paragraph", () => {
-  const output = Markdown.deserialize("This is just a sentance", {
-    terse: true
-  });
+  const output = Markdown.deserialize("This is just a sentance");
   expect(output.document.nodes).toMatchSnapshot();
 });
 
 test("parses heading1", () => {
-  const output = Markdown.deserialize("# Heading", { terse: true });
+  const output = Markdown.deserialize("# Heading");
   expect(output.document.nodes).toMatchSnapshot();
 });
 
 test("parses heading2", () => {
-  const output = Markdown.deserialize("## Heading", { terse: true });
+  const output = Markdown.deserialize("## Heading");
   expect(output.document.nodes).toMatchSnapshot();
 });
 
 test("parses heading3", () => {
-  const output = Markdown.deserialize("### Heading", { terse: true });
+  const output = Markdown.deserialize("### Heading");
   expect(output.document.nodes).toMatchSnapshot();
 });
 
 test("parses heading4", () => {
-  const output = Markdown.deserialize("#### Heading", { terse: true });
+  const output = Markdown.deserialize("#### Heading");
   expect(output.document.nodes).toMatchSnapshot();
 });
 
 test("parses heading5", () => {
-  const output = Markdown.deserialize("##### Heading", { terse: true });
+  const output = Markdown.deserialize("##### Heading");
   expect(output.document.nodes).toMatchSnapshot();
 });
 
 test("parses heading6", () => {
-  const output = Markdown.deserialize("###### Heading", { terse: true });
+  const output = Markdown.deserialize("###### Heading");
   expect(output.document.nodes).toMatchSnapshot();
 });
 
@@ -42,7 +40,7 @@ test("parses quote", () => {
   const text = `
 > this is a quote
 `;
-  const output = Markdown.deserialize(text, { terse: true });
+  const output = Markdown.deserialize(text);
   expect(output.document.nodes).toMatchSnapshot();
 });
 
@@ -50,7 +48,7 @@ test("parses quote with marks", () => {
   const text = `
 > **bold** in a quote
 `;
-  const output = Markdown.deserialize(text, { terse: true });
+  const output = Markdown.deserialize(text);
   expect(output.document.nodes).toMatchSnapshot();
 });
 
@@ -59,7 +57,7 @@ test("parses list items", () => {
 - one
 - two
 `;
-  const output = Markdown.deserialize(text, { terse: true });
+  const output = Markdown.deserialize(text);
   expect(output.document.nodes).toMatchSnapshot();
 });
 
@@ -69,7 +67,7 @@ test("parses list with trailing item", () => {
 - two
 -
 `;
-  const output = Markdown.deserialize(text, { terse: true });
+  const output = Markdown.deserialize(text);
   expect(output.document.nodes).toMatchSnapshot();
 });
 
@@ -78,7 +76,7 @@ test("parses indented list items", () => {
  - one
  - two
 `;
-  const output = Markdown.deserialize(text, { terse: true });
+  const output = Markdown.deserialize(text);
   expect(output.document.nodes).toMatchSnapshot();
 });
 
@@ -87,7 +85,7 @@ test("parses list items with marks", () => {
  - one **bold**
  - *italic* two
 `;
-  const output = Markdown.deserialize(text, { terse: true });
+  const output = Markdown.deserialize(text);
   expect(output.document.nodes).toMatchSnapshot();
 });
 
@@ -96,7 +94,7 @@ test("parses ordered list items", () => {
 1. one
 1. two
 `;
-  const output = Markdown.deserialize(text, { terse: true });
+  const output = Markdown.deserialize(text);
   expect(output.document.nodes).toMatchSnapshot();
 });
 
@@ -105,7 +103,7 @@ test("parses ordered list items with marks", () => {
 1. one **bold**
 1. *italic* two
 `;
-  const output = Markdown.deserialize(text, { terse: true });
+  const output = Markdown.deserialize(text);
   expect(output.document.nodes).toMatchSnapshot();
 });
 
@@ -115,7 +113,7 @@ test("parses ordered list items with different numbers", () => {
 2. two
 3. three
 `;
-  const output = Markdown.deserialize(text, { terse: true });
+  const output = Markdown.deserialize(text);
   expect(output.document.nodes).toMatchSnapshot();
 });
 
@@ -127,7 +125,7 @@ test("parses mixed list items", () => {
 
 1. different
 `;
-  const output = Markdown.deserialize(text, { terse: true });
+  const output = Markdown.deserialize(text);
   expect(output.document.nodes).toMatchSnapshot();
 });
 
@@ -140,9 +138,9 @@ test("parses tables", () => {
 | col 3 is | right-aligned |    $1 |
 `;
 
-  const output = Markdown.deserialize(text, { terse: true });
+  const output = Markdown.deserialize(text);
   const result = Markdown.serialize(output);
-  const output2 = Markdown.deserialize(result, { terse: true });
+  const output2 = Markdown.deserialize(result);
   expect(output2.document.nodes).toMatchSnapshot();
 });
 
@@ -151,7 +149,7 @@ test("parses todo list items", () => {
 [ ] todo
 [x] done
 `;
-  const output = Markdown.deserialize(text, { terse: true });
+  const output = Markdown.deserialize(text);
   expect(output.document.nodes).toMatchSnapshot();
 });
 
@@ -161,7 +159,7 @@ test("parses nested todo list items", () => {
    [ ] nested
    [ ] deep
 `;
-  const output = Markdown.deserialize(text, { terse: true });
+  const output = Markdown.deserialize(text);
   expect(output.document.nodes).toMatchSnapshot();
 });
 
@@ -173,7 +171,7 @@ test("parses double nested todo list items", () => {
 
 [ ] three
 `;
-  const output = Markdown.deserialize(text, { terse: true });
+  const output = Markdown.deserialize(text);
   expect(output.document.nodes).toMatchSnapshot();
 });
 
@@ -182,7 +180,7 @@ test("parses todo list items with marks", () => {
  [x] ~~done~~
  [x] more **done**
 `;
-  const output = Markdown.deserialize(text, { terse: true });
+  const output = Markdown.deserialize(text);
   expect(output.document.nodes).toMatchSnapshot();
 });
 
@@ -195,7 +193,7 @@ function() {
 }
 \`\`\`
 `;
-  const output = Markdown.deserialize(text, { terse: true });
+  const output = Markdown.deserialize(text);
   expect(output.document.nodes).toMatchSnapshot();
 });
 
@@ -208,7 +206,7 @@ function() {
 }
 ~~~
 `;
-  const output = Markdown.deserialize(text, { terse: true });
+  const output = Markdown.deserialize(text);
   expect(output.document.nodes).toMatchSnapshot();
 });
 
@@ -219,27 +217,27 @@ test("parses indented code blocks", () => {
       return hello;
     }
 `;
-  const output = Markdown.deserialize(text, { terse: true });
+  const output = Markdown.deserialize(text);
   expect(output.document.nodes).toMatchSnapshot();
 });
 
 test("parses link", () => {
   const text = `[google](http://google.com)`;
-  const output = Markdown.deserialize(text, { terse: true });
+  const output = Markdown.deserialize(text);
   expect(output.document.nodes).toMatchSnapshot();
 });
 
 test("parses link within mark", () => {
   const text = `**[google](http://google.com)**`;
-  const output = Markdown.deserialize(text, { terse: true });
+  const output = Markdown.deserialize(text);
   expect(output.document.nodes).toMatchSnapshot();
 });
 
 test("parses link with encoded characters", () => {
   const text = `[kibana](https://example.com/app/kibana#/discover?_g=%28refreshInterval:%28%27$$hashKey%27:%27object:1596%27,display:%2710%20seconds%27,pause:!f,section:1,value:10000%29,time:%28from:now-15m,mode:quick,to:now%29%29&_a=%28columns:!%28metadata.step,message,metadata.attempt_f,metadata.tries_f,metadata.error_class,metadata.url%29,index:%27logs-%27,interval:auto,query:%28query_string:%28analyze_wildcard:!t,query:%27metadata.at:%20Stepper*%27%29%29,sort:!%28time,desc%29%29)`;
-  const output = Markdown.deserialize(text, { terse: true });
+  const output = Markdown.deserialize(text);
   const result = Markdown.serialize(output);
-  const output2 = Markdown.deserialize(result, { terse: true });
+  const output2 = Markdown.deserialize(result);
   expect(output2.document.nodes).toMatchSnapshot();
 });
 
@@ -249,6 +247,16 @@ test("parses interesting nesting", () => {
 
   >Blockquote with code \`mapStateToProps()\`
 `;
-  const output = Markdown.deserialize(text, { terse: true });
+  const output = Markdown.deserialize(text);
+  expect(output.document.nodes).toMatchSnapshot();
+});
+
+test("parses empty string", () => {
+  const output = Markdown.deserialize("");
+  expect(output.document.nodes).toMatchSnapshot();
+});
+
+test("parses whitespace string", () => {
+  const output = Markdown.deserialize("   ");
   expect(output.document.nodes).toMatchSnapshot();
 });
